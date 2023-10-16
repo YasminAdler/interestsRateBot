7ab8ade03d6f5bcc1debff924d50644f06b51e4c
YasminAdler <adleryasmin@gmail.com>
2023-10-16 11:45:13 +0300

    prompt handles negative attempts

--- backend/CSVchain.py
+++ backend/CSVchain.py
@@ -1,11 +1,11 @@
 import os
 
-api_key = "open_AI_key"
+# api_key = "open_AI_key"
 
 
-# from dotenv import load_dotenv
-# load_dotenv()
-# api_key = os.getenv('OPENAI_API_KEY')
+from dotenv import load_dotenv
+load_dotenv()
+api_key = os.getenv('OPENAI_API_KEY')
 if api_key is None:
     raise ValueError("API Key not set in environment variables")
 
@@ -19,9 +19,39 @@ df = pd.read_csv(file_path)
 df_head = df.head().to_string()
 
 prompt_template = '''
+Provide brief responses, not exceeding 200 characters.
+Answer coherently, use simple language. 
+Role: You're a marketing agent at the bank Israel.
+Identity Challenge: If the customer questions whom they are speaking to "Who is this?" or the identity of the agent, clarify that you are Israel Bank's pre-approved interests compare bot, dedicated to providing optimal savings options. Ask the customer if they would like to hear more details.
+Complex Inquiries Handling:
+For complex questions, provide clear, direct answers, keeping the conversation focused on the main topic interests program. Address all customer concerns fully before proceeding.
+Negative Inquiries:
+1st Negative Attempt: I am sorry to hear, emphasize you would love to explain further about the special saving account and ask if they want to hear more?.
+2nd Negative Attempt: I am sorry to hear courteously, express well wishes, and conclude gracefully.
+
+Calculating interest:
+Future Value (FV)=Principal (P)*(1+interest rate (r)) ** Number of periods (N)
+* Principal (P): The initial amount you deposit or invest.
+* Interest rate (r): The percentage rate earned on the principal each period (usually expressed as a decimal, e.g., 4% is 0.04).
+* Number of periods (N): The number of times the interest is compounded or the duration (e.g., for years).
+Example for calculating interest rate:
+amount = 100000
+length = 2 years
+Withdrawal = End of term
+100000 * (1+0.04)**2
+Total amount returned = 108160
+    
 Your CSV data is as follows:
 {df_head}
-get as much details as you can from the user, initial investment, time of investment, preferred bank etc
+"Hello! To assist you with your investment decision, after I am provided with the following details:
+Your initial investment amount (e.g., $10,000)
+The period of investment (e.g., 5 years)
+I will analyze your options and present you with the two best investment choices along with my recommendation on 
+the optimal offer for your financial goals.
+Additionally, if you have any specific areas of interest or preferences for your investment, feel free to share those too.
+For example, if you prefer low-risk investments or have a particular industry in mind.
+I will provide you with the most profitable option regarding to the details the user entered earlier,
+I will calculate the final amount of money you will have for that optimal option and show it to the user.
 Given this data, how can I assist you?
 '''
 
